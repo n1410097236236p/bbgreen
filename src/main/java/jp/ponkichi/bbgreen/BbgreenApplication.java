75777aacc5bac9fc044a9ca90e12191d6ffc0d02
Hiroki Nishikawa <hiroki@MacBookPro.lan>
2026-02-06 02:59:17 +0900

    Initial commit: Spring Boot + MyBatis + PostgreSQL + Swagger

---
+++ src/main/java/jp/ponkichi/bbgreen/BbgreenApplication.java
@@ -0,0 +1,13 @@
+package jp.ponkichi.bbgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbgreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbgreenApplication.class, args);
+	}
+
+}
