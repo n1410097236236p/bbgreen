5cc9ca4ee0af92883195e36338e0140344d8bc37
tsayano0520-pixel <t.sayano0520@gmail.com>
2026-08-09 14:47:19 +0900

    sum関数を追加

--- src/main/java/jp/ponkichi/bbgreen/controller/HealthCheckController.java
+++ src/main/java/jp/ponkichi/bbgreen/controller/HealthCheckController.java
@@ -2,6 +2,7 @@ package jp.ponkichi.bbgreen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jp.ponkichi.bbgreen.dto.element.Message;
@@ -20,12 +21,17 @@ public class HealthCheckController {
     return new Message("Hello 38252!");
   }
 
-  // ToDo: パスパラメータとして与えられた文字列を使った Message を返す機能を追加する。
-
   @GetMapping("/api/healthCheck/freemessage/{message}")
   @SecurityRequirement(name = "")
   public Message freeMessage(@PathVariable String message) {
     return new Message(message);
+  }
+
+
+  // ToDo:整数のリストを受け取って、その合計を返す
+  @GetMapping("/api/healthCheck/sum")
+  public int sum(@RequestParam int a, @RequestParam int b) {
+    return a + b;
   }
 
   // ToDo: DBへの接続を確認できる機能を追加する
